1fc460f298d1d7a7a87e5ec0d43bd0bb052243b5
Tristan Myers <tristifano@gmail.com>
2019-08-03 14:17:42 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduciton(name)
+  puts "Hi, my name is #{name}"
+end
+
+def introduciton_with_language(name , language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduciton_with_language_optional(name , language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
